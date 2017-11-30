dc05a3c6a563ac02d0f885cfb90a5e283d39f12f
Till Bungert <tillbungert@gmail.com>
2017-11-30 02:49:04 +0100

    Tighter plots

--- test.py
+++ test.py
@@ -33,6 +33,7 @@ trainloader_pca = torch.utils.data.DataLoader(trainset,
                                           shuffle=False, num_workers=2)
 
 def imshow(img):
+    plt.figure(tight_layout=True)
     img = (img - img.min())/(img.max()-img.min())
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
@@ -48,24 +49,24 @@ images, labels = dataiter.next()
 
 # save originals
 imshow(torchvision.utils.make_grid(images))
-plt.savefig('originals.svg')
+plt.savefig('originals.svg',bbox_inches='tight')
 
 # generate and look at feature space
 m = train_mnist.main()
 out = m.no_class_forward(Variable(images).cuda())
 out2 = unsqueeze(unsqueeze(out))
 imshow(torchvision.utils.make_grid(out2.cpu().data))
-plt.savefig('featurespace.svg')
+plt.savefig('featurespace.svg',bbox_inches='tight')
 
 # invert net, regenerate input
 out3 = m.generate(out)
 imshow(torchvision.utils.make_grid(out3.cpu()))
-plt.savefig('regenerated.svg')
+plt.savefig('regenerated.svg',bbox_inches='tight')
 
 dataiter_pca = iter(trainloader_pca)
 images, labels = dataiter_pca.next()
 imshow(torchvision.utils.make_grid(images[:5]))
-plt.savefig('originals_pca.svg')
+plt.savefig('originals_pca.svg',bbox_inches='tight')
 data = m.no_class_forward(Variable(images).cuda()).cpu().data
 
 data = data.view(data.size(0), -1)
@@ -74,21 +75,21 @@ for i in range(10):
     _, indices = torch.sort(data[:,i].squeeze())
 
     imshow(torchvision.utils.make_grid(images[indices][9800:]))
-    plt.savefig('sorted{}.svg'.format(i))
+    plt.savefig('sorted{}.svg'.format(i),bbox_inches='tight')
 
 for i in range(10,15):
     print(data[:,i].min(), data[:,i].max())
     _, indices = torch.sort(data[:,i].squeeze())
 
     imshow(torchvision.utils.make_grid(images[indices][::100]))
-    plt.savefig('sorted{}.svg'.format(i))
+    plt.savefig('sorted{}.svg'.format(i),bbox_inches='tight')
 
 seed = torch.rand(1,16*7*7)
 seed[0,0] = 100
 seed[0,1:10] = -10
 seed = seed.view(1,16,7,7)
 imshow(torchvision.utils.make_grid(m.generate(Variable(seed).cuda()).cpu()))
-plt.savefig('generated.svg')
+plt.savefig('generated.svg',bbox_inches='tight')
 
 pca = PCA(15)
 pca.fit(data)
@@ -100,11 +101,11 @@ regenerated = pca.inverse_transform(trans[:5])
 regenerated.shape = -1, 16, 7, 7
 regenerated = m.generate(torch.Tensor(regenerated).cuda())
 imshow(torchvision.utils.make_grid(regenerated.cpu()))
-plt.savefig('pca.svg')
+plt.savefig('pca.svg',bbox_inches='tight')
 
 for i in range(15):
     print(trans[:,i].min(), trans[:,i].max())
     indices = np.argsort(trans[:,i])
 
     imshow(torchvision.utils.make_grid(images[torch.Tensor(indices).long()][9800:]))
-    plt.savefig('sorted_pca{}.svg'.format(i))
+    plt.savefig('sorted_pca{}.svg'.format(i),bbox_inches='tight')
