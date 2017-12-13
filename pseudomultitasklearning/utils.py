2d1f402e1399451610bea47da708543f8ada2dd5
Till Bungert <tillbungert@gmail.com>
2017-12-13 16:53:17 +0100

    Begin working on a single central train script

---
+++ pseudomultitasklearning/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def imshow(img):
+    """Plot a 4D tensor as a batch of images
+    
+    Arguments:
+        img (Tensor): The images to display
+    """
+
+    plt.figure(tight_layout=True)
+
+    img = (img - img.min())/(img.max()-img.min())
+    npimg = img.numpy()
+
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
