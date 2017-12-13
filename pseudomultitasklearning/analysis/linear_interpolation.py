da0ba9ac8bd1018c456948f0bafba382e5e01b2c
Till Bungert <tillbungert@gmail.com>
2017-12-13 16:55:02 +0100

    Take a look at interpolation between samples

---
+++ pseudomultitasklearning/analysis/linear_interpolation.py
@@ -0,0 +1,47 @@
+from pseudomultitasklearning import train
+from pseudomultitasklearning.utils import imshow
+from pseudomultitasklearning.modules.reversible import ReversibleMultiTaskNet
+
+import torch
+
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Prepare data
+
+dataloader = train.load_mnist(10000, shuffle=False, train=False)
+
+dataiter = iter(dataloader)
+images, labels = dataiter.next()
+
+ones = images.index_select(0, torch.nonzero(labels == 1).squeeze())
+
+idx = torch.LongTensor(2).random_(0, ones.size(0))
+
+samples = ones.index_select(0, idx)
+
+
+# Prepare model
+
+model = train.main()
+
+
+# Prepare features
+features = model.no_class_forward(Variable(samples).cuda())
+
+
+# Interpolate features
+steps = np.linspace(0, 1, 16)
+
+interpolation = torch.stack([t*features.data[0]+(1-t)*features.data[1] for t in steps])
+
+generated = model.generate(Variable(interpolation).cuda())
+
+imshow(torchvision.utils.make_grid(generated.cpu(), 16))
+plt.savefig('interpolation.pdf')
