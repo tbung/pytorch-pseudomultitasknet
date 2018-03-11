43c536329bcd61c4123c1a4b954f0be65831b275
Till Bungert <tillbungert@gmail.com>
2018-03-11 18:18:06 +0100

    Implement sparsity layer

--- pseudomultitasknet/modules/sparsity.py
+++ pseudomultitasknet/modules/sparsity.py
@@ -4,17 +4,44 @@ import torch.nn as nn
 
 class SparsityFunction(torch.autograd.Function):
     @staticmethod
-    def forward(x):
-        pass
+    def forward(ctx, x, Q, tau):
+        Y = Q.matmul(x.t()).t()
+
+        ctx.save_for_backward(Y, Q)
+        ctx.tau = tau
+
+        return torch.norm(Y, 1)
 
     @staticmethod
-    def backward(dy):
-        pass
+    def backward(ctx, dy):
+        Y, Q = ctx.saved_variables
+        t = ctx.tau
+
+        K = Y.size(0)
+        one = torch.eye(2*K, 2*K)
+
+        U = torch.cat((Y.sign()/(2*K), -Y))
+        V_t = torch.cat((Y, Y.sign()/(2*K))).t()
+
+        dQ = 2 * t * U.matmul((one + t * V_t.matmul(U)).inverse()).matmul(V_t).matmul(Q)
+
+        dx = Q.matmul(dy.t()).t()
+
+        return dx, dQ, None
 
 
 class SparsityBlock(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, size, tau):
+        self.tau = tau
+
+        self.register_parameter(
+            'Q',
+            nn.Parameter(torch.Tensor(size, size))
+        )
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.orthogonal(self.Q)
 
     def forward(self, x):
-        pass
+        return SparsityFunction.apply(x, self.Q, self.tau)
