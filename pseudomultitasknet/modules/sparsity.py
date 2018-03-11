e8508c471471061422adf5a7a35540f55f4fe779
Till Bungert <tillbungert@gmail.com>
2018-03-11 14:49:55 +0100

    Add skeleton of sparsity block

---
+++ pseudomultitasknet/modules/sparsity.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class SparsityFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(x):
+        pass
+
+    @staticmethod
+    def backward(dy):
+        pass
+
+
+class SparsityBlock(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
